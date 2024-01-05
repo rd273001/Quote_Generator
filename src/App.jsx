064db3e3c5f288a28c2a28bfa7cda0f9dec8f686
rd273001 <rd273001@gmail.com>
2024-01-05 20:08:19 +0530

    Initial commit - Created React app with Vite

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl font-extrabold text-purple-400'>Vite + React</h1>
+      <button
+        className='text-xl font-bold text-red-600 border-4 border-blue-400'
+      >
+          Click Here
+      </button>
+      <p className='text-lg text-violet-600'>Demo App</p>
+    </>
+  )
+};
+
+export default App
