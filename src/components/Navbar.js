ce76c51fbe85d9c33d54bce96f671f45287365e4
rd273001 <rd273001@gmail.com>
2024-05-05 17:11:13 +0530

    Revert "Added redirects file"
    This reverts commit af57b277fdaa1d237c2bfc1b5b7741ac720de601.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { BiBookmarks } from 'react-icons/bi';
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between sm:mx-10'>
+    <nav className='flex justify-between sm:mx-10 mb-4'>
       {/* used NavLink for styling active link (Home or Bookmarks) */}
       <NavLink to='/' className={ ( { isActive } ) => `${ isActive ? 'font-bold text-2xl sm:text-3xl' : 'text-lg sm:text-2xl' } hover:text-red-400` }>Home</NavLink>
       <NavLink to='/bookmarks' className={ ( { isActive } ) => `${ isActive ? 'font-bold text-2xl sm:text-3xl' : 'text-lg sm:text-2xl' } flex items-center hover:text-red-400` }>
